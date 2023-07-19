538456fa4d2f717d5cc16068dd4e5861252e1278
MitchellCaldwell29 <131213839+MitchellCaldwell29@users.noreply.github.com>
2023-07-19 10:23:10 +1000

    Folder (Audio) - Mixer and Script
    Audio Folder added with clips and mixer subfolders (with 2 volcano sounds and the bing noise)
    Script for randomizer is complete except for the key down function which has to be changed. Otherwise it's all good to go.

---
+++ Assets/Scripts/SoundRandomiser.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio; 
+
+public class SoundRandomiser : MonoBehaviour
+{
+    public AudioClip[] clips;
+    public AudioMixerGroup output;
+
+    public float minPitch = .95f;
+    public float maxPitch = 1.05f; 
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("space")) // change for arrow keys later 
+        {
+            PlaySound();
+        }
+
+    }
+
+    void PlaySound()
+    {
+        //Randomize
+        int randomClip = Random.Range (0, clips.Length);
+
+        //Create an AudioSource
+        AudioSource source = gameObject.AddComponent<AudioSource>();
+
+        //Load clip into Audiosource
+        source.clip = clips[randomClip];
+
+        // Set the output for AudioSource
+        source.outputAudioMixerGroup = output; 
+
+        // Set the pitch to randomize
+        source.pitch = Random.Range (minPitch, maxPitch);
+
+        //Play the clip
+        source.Play ();
+
+        //Destroy the AudioSource when done playing clip
+        Destroy(source, clips[randomClip].length);
+    }
+
+}
+
